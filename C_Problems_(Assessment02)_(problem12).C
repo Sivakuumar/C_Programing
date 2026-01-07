89c7987191a9305a3f9e646a22f4c1e2ea16154e
Sivakuumar <sivakumarl05042006@gmail.com>
2026-01-07 11:59:48 +0530

    c programing

---
+++ C_Problems_(Assessment02)_(problem12).C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main ()
+{
+int X,Y,A,sum;
+printf("enter number:");
+scanf("%d",&A);
+X=A/10;
+Y=A%10;
+sum=(X>=Y);
+printf("result=%d",sum);
+}
---
+++ C_Problems_(Assessment02)_(problem14).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+int X,Y,Z,B,A,sum;
+printf("enter number:");
+scanf("%d",&A);
+X=A/1000;
+Y=(A%1000)/100;
+Z=((A%100)%100)/10;
+B=(((A%10)%10)%10);
+sum=(B==Y)&&(Z==X) ;
+printf("result=%d",sum);
+}
