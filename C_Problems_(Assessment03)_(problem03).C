40adc7b61411e3c70e430356622159b86b550692
Sivakuumar <sivakumarl05042006@gmail.com>
2026-01-08 16:00:26 +0530

    c programing

---
+++ C_Problems_(Assessment03)_(problem03).C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+    int Y;
+    printf("enter number:");
+    scanf("%d",&Y);
+      if( Y<50){
+      printf("success");
+    }
+      else {
+      printf("failuer");
+    }
+    return 0;  
+}
