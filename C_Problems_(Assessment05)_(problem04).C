6daa05bec9a3df7b7742e2d981246af136bb02e7
Sivakuumar <sivakumarl05042006@gmail.com>
2026-01-22 21:46:09 +0530

    c programing

---
+++ C_Problems_(Assessment05)_(problem04).C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main ()
+{
+    int x,sum;
+    sum = 0;
+    for(x=6;x>=1;x--)
+      {
+        sum=sum + x;
+      }
+    printf("%d",sum);
+}
---
+++ C_Problems_(Assessment05)_(problem05).C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+    int x;
+    x = 1;
+    for(x=1;x<10;x++)
+      {
+        if(x%2!=0)
+      {
+    printf("%d",x);
+      }
+      }
+}
