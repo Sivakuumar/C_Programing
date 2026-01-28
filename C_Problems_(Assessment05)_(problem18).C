e33b6a228295c4ca5fb50934418802cc9f92167c
Sivakuumar <sivakumarl05042006@gmail.com>
2026-01-28 18:55:04 +0530

    c programing

---
+++ C_Problems_(Assessment05)_(problem18).C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+   int n,i,last;
+   i=2;
+   printf("Enter an number : ");
+   scanf("%d",&n);
+   last = n % 100;
+   for (;i<last;i++)
+   {
+    if( last% i == 0)
+    {
+        printf("Not Prime");
+        goto last;
+    }
+   }
+   printf("Prime");
+   last:return 0;
+}
