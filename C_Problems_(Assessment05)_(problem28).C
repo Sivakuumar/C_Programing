9ceb743ae05fbc5868daca8cd46169abb70b42b9
Sivakuumar <sivakumarl05042006@gmail.com>
2026-02-09 21:34:03 +0530

    C_Problems

--- C_Problems_(Assessment05)_(problem28).C
+++ C_Problems_(Assessment05)_(problem28).C
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,lcm;
+    int a,b,LCM;
     printf("Enter an number 1 : ");
     scanf("%d",&a);
     printf("Enter an number 2 : ");
     scanf("%d",&b);
-    for(lcm = (a<b)?a:b; ;lcm++)
+    for(lcm = (a<b)?a:b; ;LCM++)
     {
-        if((lcm % a == 0) && (lcm % b == 0))
+        if((LCM % a == 0) && (LCM % b == 0))
         {
-            printf("%d",lcm);
+            printf("%d",LCM);
             goto end;
         }
     }
