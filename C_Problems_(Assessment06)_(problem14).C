c4849c29632f784ac74ce2b86229a6a65c7e697e
Sivakuumar <sivakumarl05042006@gmail.com>
2026-02-24 20:11:06 +0530

    c programing

---
+++ C_Problems_(Assessment06)_(problem14).C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+   int n,orginal,d,last,first,mid,o;
+   d=1;
+   printf("Enter an number : ");
+   scanf("%d",&n);
+   orginal = n;
+   last = n % 10;
+    while(n>= 10)
+    {
+        d=d*10;
+        n/=10;
+    }  
+      last = last * d;
+   first = n;
+   mid = (orginal % d)/ 10;
+   o = last + (mid * 10) + first;
+   printf("%d",o);
+}
