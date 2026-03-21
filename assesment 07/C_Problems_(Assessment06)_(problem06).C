1b126449ffd3d4b5f4f1f315c5f302f2f611a505
Sivakuumar <sivakumarl05042006@gmail.com>
2026-03-21 23:11:53 +0530

    C programing

---
+++ assesment 07/C_Problems_(Assessment06)_(problem06).C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void disp_2digit_odd_below_20(int);
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    disp_2digit_odd_below_20(x);
+}
+void disp_2digit_odd_below_20(int x)
+{
+    int y ;
+    for(y=x;y<=20;y++)
+    {
+       if (y%2!=0)
+       {
+        printf("%d\n", y);
+       }
+
+    }
+    
+}
